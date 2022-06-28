55a547b8adfc5a9763421e5483a40b5ef3751c8e
caiodonatt@gmail.com <caiodonatt@gmail.com>
2022-06-28 01:45:58 -0300

    add Selenium

---
+++ laravel_php/tests/Feature/SeleniumTest.php
@@ -0,0 +1,73 @@
+<?php
+
+namespace Facebook\WebDriver;
+
+use Facebook\WebDriver\Remote\DesiredCapabilities;
+use Facebook\WebDriver\Remote\RemoteWebDriver;
+
+require_once('vendor/autoload.php');
+
+// This is where Selenium server 2/3 listens by default. For Selenium 4, Chromedriver or Geckodriver, use http://localhost:4444/
+$host = 'http://localhost:4444';
+
+$capabilities = DesiredCapabilities::chrome();
+
+$driver = RemoteWebDriver::create($host, $capabilities);
+
+$driver->get('http://127.0.0.1:8800');
+/*
+// write 'PHP' in the search box
+$driver->findElement(WebDriverBy::id('searchInput')) // find search input element
+    ->sendKeys('PHP') // fill the search box
+    ->submit(); // submit the whole form
+
+// wait until 'PHP' is shown in the page heading element
+$driver->wait()->until(
+    WebDriverExpectedCondition::elementTextContains(WebDriverBy::id('firstHeading'), 'PHP')
+);
+*/
+// print title of the current page to output
+echo "The title is '" . $driver->getTitle() . "'\n";
+
+// print URL of current page to output
+echo "The current URL is '" . $driver->getCurrentURL() . "'\n";
+
+$historyButton = $driver->findElement(WebDriverBy::id('iFrameProgramacaoCinema'));
+echo $historyButton->getText() . "'\n";
+
+$historyButton->click();
+/*
+// find element of 'History' item in menu
+$historyButton = $driver->findElement(
+    WebDriverBy::cssSelector('#ca-history a')
+);
+
+// read text of the element and print it to output
+
+// click the element to navigate to revision history page
+$historyButton->click();
+
+// wait until the target page is loaded
+$driver->wait()->until(
+    WebDriverExpectedCondition::titleContains('Revision history')
+);
+
+// print the title of the current page
+echo "The title is '" . $driver->getTitle() . "'\n";
+
+// print the URI of the current page
+
+echo "The current URI is '" . $driver->getCurrentURL() . "'\n";
+
+// delete all cookies
+$driver->manage()->deleteAllCookies();
+
+// add new cookie
+$cookie = new Cookie('cookie_set_by_selenium', 'cookie_value');
+$driver->manage()->addCookie($cookie);
+
+// dump current cookies to output
+$cookies = $driver->manage()->getCookies();
+print_r($cookies);
+*/
+// terminate the session and close the browser
